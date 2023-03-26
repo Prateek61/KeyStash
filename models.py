529b75bbb9cc1f0360460be269663ff4755bfc7a
Prateek61 <prateekpoudel61@gmail.com>
2023-03-26 19:03:30 +0545

    completed the model to store passwords

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import ForeignKey, String, Integer
-from sqlalchemy.orm import mapped_column, Mapped, relationship, DeclarativeBase
+from sqlalchemy import ForeignKey, String, Integer, create_engine
+from sqlalchemy.orm import mapped_column, Mapped, relationship, DeclarativeBase, sessionmaker, Session
+from sqlalchemy_utils import database_exists, create_database
 from cryptography.fernet import Fernet
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from typing import List, Optional, Dict
 
@@ -12,23 +14,54 @@ class Site(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     site: Mapped[str] = mapped_column(String, nullable=False)
-    username: Mapped[str] = mapped_column(String)
-    email: Mapped[str] = mapped_column(String)
+    username: Mapped[str] = mapped_column(String, nullable=True)
+    email: Mapped[str] = mapped_column(String, nullable=True)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    master: Mapped[bool] = mapped_column(Integer, nullable=False, default=0)
 
-    def __init__(self, site: str, password: str, fernet_engine: Fernet, username: Optional[str] = None, email: Optional[str] = None):
-        self.site = fernet_engine.encrypt(password.encode())
-        self.username = fernet_engine.encrypt(username.encode()) if username else None
-        self.email = fernet_engine.encrypt(email.encode()) if email else None
-        self.password = fernet_engine.encrypt(password.encode())
+    def __init__(self, site: str, password: str, username: Optional[str] = None, email: Optional[str] = None, master: Optional[bool] = False):
+        if not master:
+            self.site = site
+            self.username = username
+            self.email = email
+            self.password = password
+        else:
+            self.site = site
+            self.password = generate_password_hash(password)
 
     def __repr__(self):
         return f"<Site(site='{self.site}', email='{self.email}', username='{self.username}')>"
     
-    def decrypt(self, fernet_engine: Fernet) -> Dict[str, Optional[str]]:
-        result = dict()
-        result["site"] = fernet_engine.decrypt(self.site).decode()
-        result["username"] = fernet_engine.decrypt(self.username).decode() if self.username else None
-        result["email"] = fernet_engine.decrypt(self.email).decode() if self.email else None
-        result["password"] = fernet_engine.decrypt(self.password).decode()
+    def decode(self) -> Dict[str, str]:
+        result = {}
+        result["site"] = self.site
+        result["username"] = self.username
+        result["email"] = self.email
+        result["password"] = self.password
+        return result
 
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
+
+    @classmethod
+    def get_master(cls, session: Session):
+        return session.query(cls).filter(cls.master == 1).first()
+
+
+def get_session() -> Session:
+    engine = create_engine("sqlite:///data.db")
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    
+    Base.metadata.create_all(bind=engine)
+
+    Session_current = sessionmaker(bind=engine)
+    session_current = Session_current()
+    return session_current
+
+def setup_database():
+    engine = create_engine("sqlite:///data.db", echo=True)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    
+    Base.metadata.create_all(bind=engine)
--- project.py
+++ project.py
@@ -1,24 +1,6 @@
-#test master key='CK5VN-kImuXPKaOZy9wczEXr9-bHExfD48RwNCSEes8='
-from cryptography.fernet import Fernet
-from sqlalchemy import create_engine,Column,String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-Base=declarative_base()
-class Asite(Base):
-    __tablename__="data"
-    site=Column("site",String,primary_key=True)
-    email=Column("email",String)
-    username=Column("username",String)
-    password=Column("password",String)
+from models import Site, get_session
 
-    def __init__(self,**kwargs):
-        self.site=kwargs["site"]
-        self.email=kwargs["email"]
-        self.username=kwargs["username"]
-        self.password=kwargs["password"]
-
-    def __repr__(self):
-        return f"<Asite(site='{self.site}', email='{self.email}', username='{self.username}', password='{self.password}')>"
+SESSION = get_session()
 
 
 
@@ -34,40 +16,26 @@ def get_username():
     return input("Enter username ")
 
 def new_password():
-    key=get_key()
-    f = Fernet(key)
-    engine = create_engine("sqlite:///data.db", echo=True)
-    Base.metadata.create_all(bind=engine)
-    Session=sessionmaker(bind=engine)
-    session=Session() 
-    while True:
-        site=f.encrypt(get_site().encode())
-        email=f.encrypt(get_email().encode())
-        password=f.encrypt(get_password().encode())
-        username=f.encrypt(get_username().encode())
-        asite=Asite(site=site,email=email,password=password,username=username)
-        session.add(asite)
-        session.commit()
+    site = get_site()
+    email = get_email()
+    username = get_username()
+    password = get_password()
+
+    # Add the data to the database
+    session = get_session()
+    session.add(Site(site=site, password=password, username=username, email=email))
+    session.commit()
 
 
 def saved_password():
-    key=get_key()
-    f = Fernet(key)
-    engine = create_engine("sqlite:///data.db", echo=True)
-    Base.metadata.create_all(bind=engine)
-    Session=sessionmaker(bind=engine)
-    session=Session() 
-    vals=session.query(Asite).all()
-    for val in vals:
-        print("site=",f.decrypt(val.site).decode())
-        print("email=",f.decrypt(val.email).decode())
-        print("username=",f.decrypt(val.username).decode())
-        print("password=",f.decrypt(val.password).decode())
+    return None
 
 
     
 
 def main() -> None:
+    new_password()
+    return
     x=input("n=new-password s=saved-password ")
     if x=="n":
         new_password()
