cab93abb22e859c329c84b0d89f3701eab3fb141
Prateek61 <prateekpoudel61@gmail.com>
2023-04-27 23:24:23 +0545

    added typer and rich

--- project.py
+++ project.py
@@ -1,25 +1,55 @@
 from typing import Optional
 
+import typer
+from rich import print as rprint
+
 from models import Site
 from database import SESSION
+from werkzeug.security import check_password_hash
+
+app = typer.Typer()
+
+
+def get_master_password() -> str:
+    master = SESSION.query(Site).filter(Site.master == 1).first()
+
+    if not master:
+        rprint("[bold red]Master password not set[/bold red]")
+        rprint("[bold red]Run: [/bold red][bold green]setup[/bold green]")
+        raise typer.Exit()
+    
+    while True:
+        password = typer.prompt("Enter master password", hide_input=True)
+        if check_password_hash(master.password, password):
+            return password
+        else:
+            typer.echo("Incorrect password")
+
+
+@app.command()
+def setup():
+    master = SESSION.query(Site).filter(Site.master == 1).first()
+    if master:
+        print("Master password already set.")
+        return
+    else:
+        username = typer.prompt("Enter master username")
+        password = typer.prompt("Enter master password", hide_input=True)
+
+        master_site = Site(site="master", password=password, username=username, master=True)
+        SESSION.add(master_site)
+        SESSION.commit()
+
+
+#@authentication_required
+@app.command()
+def check():
+    password = get_master_password()
+    print(password)
+
 
 def main() -> None:
-
-    master = SESSION.query(Site).filter(Site.master == 1).first()
-    if master is None:
-        print("No master password set. Please set a master password.")
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-
-        master_site = Site("master", password, username, True)
-        SESSION.add(master_site)
-        SESSION.commit()   
-    else:
-        password = input("Enter master password: ")
-        while not master.check_password(password):
-            password = input("Incorrect password. Enter master password: ")
-
-    # TODO
+    app()
 
 
 if __name__ == "__main__":
